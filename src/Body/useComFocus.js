37a15c917899a4c8e3c5c57810f3e0c153205bf4
Dripsama <nikhilfacebook9@gmail.com>
2021-05-07 13:14:12 +0530

    Fixed the dependency issue

--- src/Body/useComFocus.js
+++ src/Body/useComFocus.js
@@ -1,18 +1,21 @@
-import { useEffect } from "react";
+import { useCallback, useEffect } from "react";
 
 export default function useComFocus(node, callback) {
-  const handleClick = (event) => {
-    if (node.current.contains(event.target)) {
-      return;
-    }
-    callback();
-  };
-
+  //useCallback so that it doesn’t change on every render unless its own dependencies(node, callback) change:
+  const handleClick = useCallback(
+    (event) => {
+      if (node.current.contains(event.target)) {
+        return;
+      }
+      callback();
+    },
+    [callback, node]
+  );
   //incase of clicking outside of the the currently focused item
   useEffect(() => {
     document.addEventListener("mousedown", handleClick);
     return () => {
       document.removeEventListener("mousedown", handleClick);
     };
-  }, []);
+  }, [handleClick]);
 }
