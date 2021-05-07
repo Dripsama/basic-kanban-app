3d4a6e74a8bed8aff5e86071e169a2bd28b10bee
Dripsama <nikhilfacebook9@gmail.com>
2021-05-07 19:57:35 +0530

    tasks

--- src/Body/InitialData.js
+++ src/Body/InitialData.js
@@ -1,28 +1,39 @@
 const initialData = {
   tasks: {
-    "task-1": { id: "task-1", content: "Take out the garbage" },
-    "task-2": { id: "task-2", content: "Watch my favorite show" },
-    "task-3": { id: "task-3", content: "Charge my phone" },
-    "task-4": { id: "task-4", content: "Cook dinner" },
+    "task-1": { id: "task-1", content: "Sample task 1" },
+    "task-2": { id: "task-2", content: "Sample task 2" },
+    "task-3": { id: "task-3", content: "Sample task 3" },
+    "task-4": { id: "task-4", content: "Sample task 4" },
+    "task-5": { id: "task-5", content: "Sample task 5" },
   },
   columns: {
     "column-1": {
       id: "column-1",
-      title: "To do",
-      taskIds: ["task-1", "task-2", "task-3", "task-4"],
+      title: "Backlog",
+      taskIds: ["task-1"],
     },
     "column-2": {
       id: "column-2",
-      title: "In progress",
-      taskIds: [],
+      title: "To do",
+      taskIds: ["task-2"],
     },
     "column-3": {
+      id: "column-3",
+      title: "In Progress",
+      taskIds: ["task-3", "task-4"],
+    },
+    "column-4": {
+      id: "column-3",
+      title: "Testing",
+      taskIds: ["task-5"],
+    },
+    "column-5": {
       id: "column-3",
       title: "Done",
       taskIds: [],
     },
   },
-  columnOrder: ["column-1", "column-2", "column-3"],
+  columnOrder: ["column-1", "column-2", "column-3", "column-4", "column-5"],
 };
 
 export default initialData;
